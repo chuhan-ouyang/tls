2ce908cba7ad264e00e16f382fdb52a1ff930ba6
chuhan <chuhan.ouyang@gmail.com>
2024-04-17 12:47:22 -0400

    Fixed key

--- https-client/main.c
+++ https-client/main.c
@@ -17,50 +17,6 @@ static const char *s_url = "https://127.0.0.1:8443";
 static const char *s_post_data = NULL;      // POST data
 static const uint64_t s_timeout_ms = 1500;  // Connect timeout in milliseconds
 
-// ca.crt
-static const char *s_tls_ca =
-"-----BEGIN CERTIFICATE-----\n"
-"MIIFazCCA1OgAwIBAgIUE0YHFEKQXunI0C2/uKfjt2I+NeowDQYJKoZIhvcNAQEL\n"
-"BQAwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoM\n"
-"GEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0yNDA0MTcwMDAwMzBaFw0yNTA0\n"
-"MTcwMDAwMzBaMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEw\n"
-"HwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwggIiMA0GCSqGSIb3DQEB\n"
-"AQUAA4ICDwAwggIKAoICAQCx2LUx+FkcMN4dQtSJzF7+ZPc3PswccP0AKpvDWh7w\n"
-"XNJnq1gwpZlUljYPRFUvNBKcWGeoPRtiSU17xn8WQFXo4l5FwAKnZje2tGQcOFex\n"
-"DS934GJQ6ozATb0yqPHfeAvq5B72AfiXX5pA5ezCmkLPVuIZZHwOHH8JrkmgLyyB\n"
-"mXE5Kp3v7LksYMqsckm38EvwvH/sxWQwOmzgZsPCjehgGA+2c5rV5qwIMX4Wuclm\n"
-"cQX1uJNBxsA6jwcFyMRKAUN8uldZZZ5VD+iuvexSXcrX5zkG+2R62g63PdznzUII\n"
-"xs94sSHPJg7nHCcqlozKj2D7jl04cR0xwsUy+pJtd9wz1rHbtchwdS6inPa0bQWa\n"
-"ShRG5HS2IbX+Jjr0Yz2A275NehiuydYNPXyEnUDsl5+CAc/EOnLrADJbWcwVb/45\n"
-"G0qWkV6tjYGySWz2Ol2m7CQUATDD9x1udSHMBxS4Pb3GmGFqahQiNzO5eu1mkuqN\n"
-"KYGc320wfvrMWxKaTyvwWDzhH44a+QK6Kog9U6/9nL5j8sb8S41+QowWnBIYEl5e\n"
-"mS02FD8Q/L0FI3aXKUJLk+xU+1tsyCZEwRPgvmZWVAx20UbKteknKm6EJGn3/Eor\n"
-"KBGlDXJB0xTuQ4PcVO4ZSBC9xm1d17gADYi6xUP1rfSVfXPykvSPRVph0xya0Yvd\n"
-"UQIDAQABo1MwUTAdBgNVHQ4EFgQU97N0aYJb6OF6i6yCPfHuTtosbtkwHwYDVR0j\n"
-"BBgwFoAU97N0aYJb6OF6i6yCPfHuTtosbtkwDwYDVR0TAQH/BAUwAwEB/zANBgkq\n"
-"hkiG9w0BAQsFAAOCAgEAZZwZ51nNWeFdmU/keNE/yi3XN5mYTnuwfSBLFRXK85gL\n"
-"sMUC3GZDDSTCq600gOxGTBU/co592dy/LqLpmzDNnNqcAs2n95D/5UdRFEkaxmPx\n"
-"x+SJFBdrpAGQMzkLWpsTtw7eh6BVGkP9o537HI/KIrQ1+9Pc/XoIk+axdE28jutc\n"
-"lRYqzUOdPKDyK7YmqEtxI27zPT5yLOVMT68kpqNZlBcEzU7yaF6qf/BXSKbhf8xr\n"
-"hk6j8D530OaQdLUCSDZs/fWN/5M8YniNdZ9Ij+jU0CkbXg8kIlfPSYU3+NNDHeTG\n"
-"WPP8m/VVzW3y68yrUj9dqBbh+mKTCuhL5KQde3CUOoF2tMS5iaoHjkdUARATDWtg\n"
-"NfoiHpKfYEDz0XTnyQ2Pgc3ePq5ntumZx5wCVxRlTuF2JvYoHj2A5PYDY/bs0Yoe\n"
-"zzJo9fd3mb5aMtch3zY/uTo+VeHmysh8hK8iAXTY1za2MKjnGrcrrbK5TYKX3iIN\n"
-"hOYkfy6WMNXUDwHr2rdxarD8vIqBmluL1GCaoyQHRK6a2Qqa71HKu2FRNw3KyuH2\n"
-"+9JWA12BAX8kR7RUx7lj3cRk0V+vjW7g8B08PJtL1DGC8W4MLg6H09AFU+97q+LA\n"
-"y6cuOSITLDq2QvGI5Qrd1jbj/gl+3SCfkfaQfp1UvdE6HnnsP/qLj6ynkW/b9AY=\n"
-"-----END CERTIFICATE-----\n";
-
-#ifdef TLS_TWOWAY
-// client.crt
-static const char *s_tls_cert =
-"";
-
-// client.key
-static const char *s_tls_key =
-"";
-#endif
-
 // Print HTTP response and signal that we're done
 static void fn(struct mg_connection *c, int ev, void *ev_data) {
   if (ev == MG_EV_OPEN) {
